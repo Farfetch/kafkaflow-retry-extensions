e26baeff7fd3c47612a7a5a68b62e4fd9d773132
Martinho Novais <martinho.novais@farfetch.com>
2021-09-27 13:00:08 +0100

    feat: rearrange sample project

---
+++ samples/KafkaFlow.Retry.Sample/Exceptions/RetryDurableTestException.cs
@@ -0,0 +1,10 @@
+﻿namespace KafkaFlow.Retry.Sample.Exceptions
+{
+    using System;
+
+    internal class RetryDurableTestException : Exception
+    {
+        public RetryDurableTestException(string message) : base(message)
+        { }
+    }
+}
