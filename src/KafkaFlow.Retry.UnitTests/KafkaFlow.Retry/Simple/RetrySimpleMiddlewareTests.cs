d7a2eaae7e8713c9f4bd17e9850acb5e6aca57a6
Sérgio Ribeiro <sergio.ribeiro@farfetch.com>
2021-10-22 15:37:41 +0100

    chore: change unit test to validate coverage in codacy app

--- src/KafkaFlow.Retry.UnitTests/KafkaFlow.Retry/Simple/RetrySimpleMiddlewareTests.cs
+++ src/KafkaFlow.Retry.UnitTests/KafkaFlow.Retry/Simple/RetrySimpleMiddlewareTests.cs
@@ -2,17 +2,21 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.Diagnostics.CodeAnalysis;
+    using System.Threading;
     using System.Threading.Tasks;
     using global::KafkaFlow.Retry.Simple;
     using Moq;
     using Xunit;
 
+    [ExcludeFromCodeCoverage]
     public class RetrySimpleMiddlewareTests
     {
-        [Fact(Skip = "needs to be constructed")]
-        public async Task RetrySimpleMiddleware_Ctor_Tests()
+        [Fact]
+        public async Task RetrySimpleMiddleware_Invoke_Successfully()
         {
             //Arrange
+            string expectedConsumerName = "ConsumerName";
             Mock<ILogHandler> mockILogHandler = new Mock<ILogHandler>();
             var retrySimpleDefinition = new RetrySimpleDefinition(1, Mock.Of<IReadOnlyCollection<Func<RetryContext, bool>>>(), false, (a) => { return TimeSpan.FromSeconds(1); });
 
@@ -21,13 +25,41 @@
                 retrySimpleDefinition
                 );
 
+            Mock<IConsumerContext> mockIConsumerContext = new Mock<IConsumerContext>();
+            mockIConsumerContext
+                .SetupGet(ctx => ctx.WorkerId)
+                .Returns(1);
+            mockIConsumerContext
+                .SetupGet(ctx => ctx.ConsumerName)
+                .Returns(expectedConsumerName);
+            mockIConsumerContext
+                .SetupGet(ctx => ctx.GroupId)
+                .Returns("GroupId");
+            mockIConsumerContext
+                .SetupGet(ctx => ctx.Partition)
+                .Returns(2);
+            mockIConsumerContext
+                .SetupGet(ctx => ctx.WorkerStopped)
+                .Returns(CancellationToken.None);
+
             Mock<IMessageContext> mockIMessageContext = new Mock<IMessageContext>();
+            mockIMessageContext
+                .Setup(ctx => ctx.ConsumerContext)
+                .Returns(mockIConsumerContext.Object);
+
+            string actualConsumerName = null;
+            MiddlewareDelegate middlewareDelegate = delegate (IMessageContext context)
+            {
+                Console.WriteLine($"Notification received for: {context.Message.Key}");
+                actualConsumerName = context.ConsumerContext.ConsumerName;
+                return Task.CompletedTask;
+            };
 
             //Act
-            await retrySimpleMiddleware.Invoke(mockIMessageContext.Object, null);
+            await retrySimpleMiddleware.Invoke(mockIMessageContext.Object, middlewareDelegate);
 
             // Assert
-            Assert.True(true);
+            Assert.Equal(expectedConsumerName, actualConsumerName);
         }
     }
 }
