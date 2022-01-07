f4d42d512eb425c5df4d6342b8e1aa88c5e92f76
FARFETCH\fernando.marins <fernando.marins@farfetch.com>
2022-01-07 15:10:43 +0000

    chore: reverting integration tests deletion
    This reverts commit f6681c44560479e8bf1826eb883a9de3a9039076.

---
+++ src/KafkaFlow.Retry.IntegrationTests/RetryDurableTests.cs
@@ -0,0 +1,130 @@
+namespace KafkaFlow.Retry.IntegrationTests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using AutoFixture;
+    using KafkaFlow.Retry.IntegrationTests.Core.Bootstrappers.Fixtures;
+    using KafkaFlow.Retry.IntegrationTests.Core.Messages;
+    using KafkaFlow.Retry.IntegrationTests.Core.Producers;
+    using KafkaFlow.Retry.IntegrationTests.Core.Storages;
+    using KafkaFlow.Retry.IntegrationTests.Core.Storages.Assertion;
+    using KafkaFlow.Retry.IntegrationTests.Core.Storages.Repositories;
+    using Microsoft.Extensions.DependencyInjection;
+    using Xunit;
+
+    [Collection("BootstrapperHostCollection")]
+    public class RetryDurableTests
+    {
+        private readonly Fixture fixture = new Fixture();
+        private readonly IRepositoryProvider repositoryProvider;
+        private readonly IServiceProvider serviceProvider;
+
+        public RetryDurableTests(BootstrapperHostFixture bootstrapperHostFixture)
+        {
+            this.serviceProvider = bootstrapperHostFixture.ServiceProvider;
+            this.repositoryProvider = bootstrapperHostFixture.ServiceProvider.GetRequiredService<IRepositoryProvider>();
+            InMemoryAuxiliarStorage<RetryDurableTestMessage>.Clear();
+            InMemoryAuxiliarStorage<RetryDurableTestMessage>.ThrowException = true;
+        }
+
+        public static IEnumerable<object[]> Scenarios()
+        {
+            yield return new object[]
+            {
+                RepositoryType.MongoDb,
+                typeof(IMessageProducer<RetryDurableGuaranteeOrderedConsumptionMongoDbProducer>),
+                typeof(RetryDurableGuaranteeOrderedConsumptionPhysicalStorageAssert),
+                10
+            };
+            yield return new object[]
+            {
+                RepositoryType.SqlServer,
+                typeof(IMessageProducer<RetryDurableGuaranteeOrderedConsumptionSqlServerProducer>),
+                typeof(RetryDurableGuaranteeOrderedConsumptionPhysicalStorageAssert),
+                10
+            };
+            yield return new object[]
+            {
+                RepositoryType.MongoDb,
+                typeof(IMessageProducer<RetryDurableLatestConsumptionMongoDbProducer>),
+                typeof(RetryDurableLatestConsumptionPhysicalStorageAssert),
+                1
+            };
+            yield return new object[]
+            {
+                RepositoryType.SqlServer,
+                typeof(IMessageProducer<RetryDurableLatestConsumptionSqlServerProducer>),
+                typeof(RetryDurableLatestConsumptionPhysicalStorageAssert),
+                1
+            };
+        }
+
+        [Theory]
+        [MemberData(nameof(Scenarios))]
+        internal async Task RetryDurableTest(
+            RepositoryType repositoryType,
+            Type producerType,
+            Type physicalStorageType,
+            int numberOfTimesThatEachMessageIsTriedWhenDone)
+        {
+            // Arrange
+            var numberOfMessages = 5;
+            var numberOfMessagesByEachSameKey = 10;
+            var numberOfTimesThatEachMessageIsTriedBeforeDurable = 4;
+            var numberOfTimesThatEachMessageIsTriedDuringDurable = 2;
+            var producer = this.serviceProvider.GetRequiredService(producerType) as IMessageProducer;
+            var physicalStorageAssert = this.serviceProvider.GetRequiredService(physicalStorageType) as IPhysicalStorageAssert;
+            var messages = this.fixture.CreateMany<RetryDurableTestMessage>(numberOfMessages).ToList();
+            await this.repositoryProvider.GetRepositoryOfType(repositoryType).CleanDatabaseAsync().ConfigureAwait(false);
+            // Act
+            messages.ForEach(
+                m =>
+                {
+                    for (int i = 0; i < numberOfMessagesByEachSameKey; i++)
+                    {
+                        producer.Produce(m.Key, m);
+                    }
+                });
+
+            // Assert - Creation
+            foreach (var message in messages)
+            {
+                await InMemoryAuxiliarStorage<RetryDurableTestMessage>.AssertCountMessageAsync(message, numberOfTimesThatEachMessageIsTriedBeforeDurable).ConfigureAwait(false);
+            }
+
+            foreach (var message in messages)
+            {
+                await physicalStorageAssert.AssertRetryDurableMessageCreationAsync(repositoryType, message, numberOfMessagesByEachSameKey).ConfigureAwait(false);
+            }
+
+            // Assert - Retrying
+            InMemoryAuxiliarStorage<RetryDurableTestMessage>.Clear();
+
+            foreach (var message in messages)
+            {
+                await InMemoryAuxiliarStorage<RetryDurableTestMessage>.AssertCountMessageAsync(message, numberOfTimesThatEachMessageIsTriedDuringDurable).ConfigureAwait(false);
+            }
+
+            foreach (var message in messages)
+            {
+                await physicalStorageAssert.AssertRetryDurableMessageRetryingAsync(repositoryType, message, numberOfTimesThatEachMessageIsTriedDuringDurable).ConfigureAwait(false);
+            }
+
+            // Assert - Done
+            InMemoryAuxiliarStorage<RetryDurableTestMessage>.ThrowException = false;
+            InMemoryAuxiliarStorage<RetryDurableTestMessage>.Clear();
+
+            foreach (var message in messages)
+            {
+                await InMemoryAuxiliarStorage<RetryDurableTestMessage>.AssertCountMessageAsync(message, numberOfTimesThatEachMessageIsTriedWhenDone).ConfigureAwait(false);
+            }
+
+            foreach (var message in messages)
+            {
+                await physicalStorageAssert.AssertRetryDurableMessageDoneAsync(repositoryType, message).ConfigureAwait(false);
+            }
+        }
+    }
+}
