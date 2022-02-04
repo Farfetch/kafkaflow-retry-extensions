44b98f87fd68dc97029ea7bf04e77e5c59e3c580
Martinho Novais <martinho.novais@farfetch.com>
2022-02-04 16:21:25 +0000

    chore: add avro sample

---
+++ samples/KafkaFlow.Retry.Common.Sample/Helpers/KafkaHelper.cs
@@ -0,0 +1,65 @@
+﻿namespace KafkaFlow.Retry.Common.Sample.Helpers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    public static class KafkaHelper
+    {
+        public static async Task CreateKafkaTopics(string kafkaBrokers, string[] topics)
+        {
+            using (var adminClient = new Confluent.Kafka.AdminClientBuilder(new Confluent.Kafka.AdminClientConfig { BootstrapServers = kafkaBrokers }).Build())
+            {
+                foreach (var topic in topics)
+                {
+                    var topicMetadata = adminClient.GetMetadata(topic, TimeSpan.FromSeconds(20));
+                    if (topicMetadata.Topics.First().Partitions.Count > 0)
+                    {
+                        try
+                        {
+                            var deleteTopicRecords = new List<Confluent.Kafka.TopicPartitionOffset>();
+                            for (int i = 0; i < topicMetadata.Topics.First().Partitions.Count; i++)
+                            {
+                                deleteTopicRecords.Add(new Confluent.Kafka.TopicPartitionOffset(topic, i, Confluent.Kafka.Offset.End));
+                            }
+                            await adminClient.DeleteRecordsAsync(deleteTopicRecords).ConfigureAwait(false);
+                        }
+                        catch (Confluent.Kafka.Admin.DeleteRecordsException e)
+                        {
+                            Console.WriteLine($"An error occured deleting topic records: {e.Results[0].Error.Reason}");
+                        }
+                    }
+                    else
+                    {
+                        try
+                        {
+                            await adminClient
+                                .CreatePartitionsAsync(
+                                    new List<Confluent.Kafka.Admin.PartitionsSpecification>
+                                    {
+                                        new Confluent.Kafka.Admin.PartitionsSpecification
+                                        {
+                                            Topic = topic,
+                                            IncreaseTo = 6
+                                        }
+                                    })
+                                .ConfigureAwait(false);
+                        }
+                        catch (Confluent.Kafka.Admin.CreateTopicsException e)
+                        {
+                            if (e.Results[0].Error.Code != Confluent.Kafka.ErrorCode.UnknownTopicOrPart)
+                            {
+                                Console.WriteLine($"An error occured creating a topic: {e.Results[0].Error.Reason}");
+                            }
+                            else
+                            {
+                                Console.WriteLine("Topic does not exists");
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ samples/KafkaFlow.Retry.SchemaRegistry.Sample/Helpers/KafkaClusterConfigurationBuilderHelper.cs
@@ -0,0 +1,99 @@
+﻿namespace KafkaFlow.Retry.SchemaRegistry.Sample.Helpers
+{
+    using System;
+    using Confluent.SchemaRegistry;
+    using Confluent.SchemaRegistry.Serdes;
+    using global::SchemaRegistry;
+    using KafkaFlow.Configuration;
+    using KafkaFlow.Retry.MongoDb;
+    using KafkaFlow.Retry.SchemaRegistry.Sample.Exceptions;
+    using KafkaFlow.Retry.SchemaRegistry.Sample.Handlers;
+    using KafkaFlow.TypedHandler;
+
+    internal static class KafkaClusterConfigurationBuilderHelper
+    {
+        internal static IClusterConfigurationBuilder SetupRetryDurableMongoAvroDb(
+            this IClusterConfigurationBuilder cluster,
+            string mongoDbConnectionString,
+            string mongoDbDatabaseName,
+            string mongoDbRetryQueueCollectionName,
+            string mongoDbRetryQueueItemCollectionName)
+        {
+            cluster
+                .AddProducer(
+                    "kafka-flow-retry-durable-mongodb-avro-producer",
+                    producer => producer
+                        .DefaultTopic("sample-kafka-flow-retry-durable-mongodb-avro-topic")
+                        .WithCompression(Confluent.Kafka.CompressionType.Gzip)
+                        .AddMiddlewares(
+                            middlewares => middlewares
+                                .AddSchemaRegistryAvroSerializer(
+                                    new AvroSerializerConfig
+                                    {
+                                        SubjectNameStrategy = SubjectNameStrategy.TopicRecord
+                                    })
+                        )
+                        .WithAcks(Acks.All)
+                )
+                .AddConsumer(
+                    consumer => consumer
+                        .Topic("sample-kafka-flow-retry-durable-mongodb-avro-topic")
+                        .WithGroupId("sample-consumer-kafka-flow-retry-durable-mongodb-avro")
+                        .WithName("kafka-flow-retry-durable-mongodb-avro-consumer")
+                        .WithBufferSize(10)
+                        .WithWorkersCount(20)
+                        .WithAutoOffsetReset(AutoOffsetReset.Latest)
+                        .AddMiddlewares(
+                            middlewares => middlewares
+                                .AddSchemaRegistryAvroSerializer()
+                                .RetryDurable(
+                                    configure => configure
+                                        .Handle<RetryDurableTestException>()
+                                        .WithMessageType(typeof(AvroLogMessage))
+                                        .WithMongoDbDataProvider(
+                                            mongoDbConnectionString,
+                                            mongoDbDatabaseName,
+                                            mongoDbRetryQueueCollectionName,
+                                            mongoDbRetryQueueItemCollectionName)
+                                        .WithRetryPlanBeforeRetryDurable(
+                                            configure => configure
+                                                .TryTimes(3)
+                                                .WithTimeBetweenTriesPlan(
+                                                    TimeSpan.FromMilliseconds(250),
+                                                    TimeSpan.FromMilliseconds(500),
+                                                    TimeSpan.FromMilliseconds(1000))
+                                                .ShouldPauseConsumer(false)
+                                        )
+                                        .WithEmbeddedRetryCluster(
+                                            cluster,
+                                            configure => configure
+                                                .WithRetryTopicName("sample-kafka-flow-retry-durable-mongodb-avro-topic-retry")
+                                                .WithRetryConsumerBufferSize(4)
+                                                .WithRetryConsumerWorkersCount(2)
+                                                .WithRetryConsumerStrategy(RetryConsumerStrategy.GuaranteeOrderedConsumption)
+                                                .WithRetryTypedHandlers(
+                                                    handlers => handlers
+                                                        .WithHandlerLifetime(InstanceLifetime.Transient)
+                                                        .AddHandler<AvroMessageTestHandler>()
+                                                )
+                                                .Enabled(true)
+                                        )
+                                        .WithQueuePollingJobConfiguration(
+                                            configure => configure
+                                                .WithId("retry-durable-mongodb-avro-polling-id")
+                                                .WithCronExpression("0 0/1 * 1/1 * ? *")
+                                                .WithExpirationIntervalFactor(1)
+                                                .WithFetchSize(10)
+                                                .Enabled(true)
+                                        ))
+                                .AddTypedHandlers(
+                                    handlers => handlers
+                                        .WithHandlerLifetime(InstanceLifetime.Transient)
+                                        .AddHandler<AvroMessageTestHandler>())
+                        )
+                );
+
+            return cluster;
+        }
+    }
+}
