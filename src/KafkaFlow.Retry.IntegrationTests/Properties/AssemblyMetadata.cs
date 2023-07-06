7e5cd3b6fe80737a4d5a4326be6717a52dce1c8f
fernando.marins <fernando.marins@farfetch.com>
2023-07-06 11:00:48 +0100

    chore: avoiding parallelization for integration tests

--- src/KafkaFlow.Retry.IntegrationTests/Properties/AssemblyMetadata.cs
+++ src/KafkaFlow.Retry.IntegrationTests/Properties/AssemblyMetadata.cs
@@ -1,5 +1,7 @@
 ﻿using System.Diagnostics.CodeAnalysis;
 using System.Runtime.CompilerServices;
+using Xunit;
 
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
 [assembly: ExcludeFromCodeCoverage]
+[assembly: CollectionBehavior(DisableTestParallelization = true)]
